05ee81080fd2456403feb880d1533500b4893943
snowlxy1202 <snowlxy1202@gmail.com>
2024-01-04 19:20:40 -0800

    finalized handlebars structure

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,12 @@
 const router = require('express').Router();
 
 const createRouter = require('./api/create');
-const newRouter = require('./api/suggest');
 const storedRouter = require('./api/stored');
 const userRouter = require('./api/user');
 
 
 
 router.use('/create', createRouter);
-router.use('/new', newRouter);
 router.use('/stored', storedRouter);
 router.use('/user', userRouter);
 
