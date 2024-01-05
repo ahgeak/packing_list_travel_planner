f0c124ecdda2577ec062916bdb8059159ed8d483
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2024-01-04 18:28:38 -0800

    created config/connection.js file with JawsDB and localhost

--- models/List.js
+++ models/List.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection'); // connect this once created
+const sequelize = require('../config/connection');
 
 class List extends Model {}
 
