f11f7d96a6b26d8aa89c7d501b47255b03f301af
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2024-01-08 20:57:09 -0800

    started creating the route for rendering the stored list to /items route

--- controllers/api/items.js
+++ controllers/api/items.js
@@ -1,10 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 // create_list get request will render the create list page
-router.get('/', async (req, res) => {
-    return res.render('items');
-  });
+router.get("/", async (req, res) => {
+  return res.render("items");
+});
 
-  // we will need a UUID for each unique page for this
+// this will be used to get the ID and render the list on the screen
+router.get("/:id", async (req, res) => {
+  // List.query
+  // response.render the specific item to item handlebar
+  return res.render("items");
+});
+
+// we will need a UUID for each unique page for this
 
 module.exports = router;
---
+++ public/js/storeList.js
@@ -0,0 +1,8 @@
+const renderStoredList = async (event) => {
+    const name = "test"; //add the name of the stored list name here
+    const items = "test"; //add the array of items that will be iterated through to be rendered on the page
+
+    console.log(name);
+    console.log(items);
+
+};
