18b45c26076f887bad21091cba47c9b9386e9a02
snowlxy1202 <snowlxy1202@gmail.com>
2024-01-10 19:39:34 -0800

    modified addbtn function in create page debugged seeds

---
+++ models/test.js
@@ -0,0 +1,10 @@
+const items = ["towels", "hat","shoes","soap"]
+const items = [towel, sandals, hat, sunscreen, beachchair, surfboard]
+ // {
+  //   "name": "Toothbrush",
+  //   "items": "[tent, sleeping bag, tarp, mugs, plates]"
+  // },
+  // {
+  //   "name": "Shampoo",
+  //   "items": "[hat, gloves, jacket, scarf, snowboard, helmet]"
+  // },
--- public/js/createList.js
+++ public/js/createList.js
@@ -4,11 +4,16 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector("#list_name").value.trim();
-  const items = document
-    .querySelector("#input_items")
-    .value.trim();
-
+  let items = JSON.parse(localStorage.getItem("items")) || [];
+  
+  items = items.map((item) => item.individual);
     console.log(name, items);
+
+    window.onload = function () {
+      loadItems();
+    };
+    
+   
 
   if (name && items) {
     const response = await fetch(`/api/stored`, {
@@ -22,15 +27,69 @@ const newFormHandler = async (event) => {
     if (response.ok) {
         console.log(response);
       document.location.replace("/api/stored");
+      localStorage.clear();
+      let itemsContainer = document.getElementById("items-container");
+      itemsContainer.replaceChildren();
+
     } else {
       alert("Failed to create list");
     }
   }
 };
 
-if (document.querySelector(".new-list-form")) {
+ // function for adding itemss to create page
+ function addItem() {
+  console.log("enter addItem function");
+  let individual = document.getElementById("textarea1").value;
+  console.log(individual);
+  if (individual) {
+    // Create a item object
+    var item = {
+      individual: individual
+    };
+
+    // Get existing items from local storage
+    const items = JSON.parse(localStorage.getItem("items")) || [];
+
+    // Add the new item to the array
+    items.push(item);
+    console.log(items, "outside loaditems");
+    console.log(item);
+    // Save the updated items back to local storage
+    localStorage.setItem("items", JSON.stringify(items));
+
+
+    document.getElementById("item-form").reset();
+
+   
+    loadItems();
+  }
+}
+
+
+function loadItems() {
+  let itemsContainer = document.getElementById("items-container");
+  let items = JSON.parse(localStorage.getItem("items")) || [];
+  console.log(items, "inside loaditems");
+ 
+  itemsContainer.innerHTML = "";
+
+
+  items.forEach(function (item) {
+    let div = document.createElement("div");
+    div.className = "item";
+    div.innerHTML = item.individual;
+    itemsContainer.appendChild(div);
+  });
+}
+
+
+
+
+
+if (document.querySelector("#add-list")) {
     document
-    .querySelector(".new-list-form")
-    .addEventListener("submit", newFormHandler);
+    .querySelector("#add-list")
+    .addEventListener("click", newFormHandler);
 };
 
